b83c349cd8c1713dfe419faa32c4a6dd0f026490
Alvintol <alvin.tol@hotmail.com>
2022-03-16 15:21:15 +0000

    created client.js file to refractor play.js and begin separating functions and module building

--- play.js
+++ play.js
@@ -1,27 +1,7 @@
 const { Socket } = require("dgram");
 const net = require("net");
+const { connect } = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log('you are inside me');
-  });
-  
-  conn.setTimeout(3000);
-  conn.on('timeout', () => {
-    console.log('game timeout');
-    conn.end();
-  });
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
