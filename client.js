439479d7946d6539a3f329c20376ad1db66c01ca
Alvintol <alvin.tol@hotmail.com>
2022-03-16 16:11:03 +0000

    tested out hard coded move commands upon server connect

--- client.js
+++ client.js
@@ -12,9 +12,15 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log('you are inside me');
+    conn.write('Name: AT');
+    console.log('You are inside me...');
+    // conn.write('Move: up');
+    // conn.write('Move: left');
+    // conn.write('Move: up');
+    // conn.write('Move: left');
   });
   
+
   conn.setTimeout(3000);
   conn.on('timeout', () => {
     console.log('game timeout');
